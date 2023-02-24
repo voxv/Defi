2fe51bc5589615aac1c386e7fa8d8908dec9abe3
voxv <53357469+voxv@users.noreply.github.com>
2023-02-24 00:58:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/healthcheck', (req, res) => {
 //const PORT2 = process.env.PORT || 8000;
 app.listen(3000, () => {
   console.log(`Server listening on port 3000`);
-});*/
+});
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/card.html');
 });
