3aba34f86784a23668390218c483bb3b2f9807b7
blaboazo <blop11ca@yahoo.ca>
2023-04-01 13:15:04 -0400

    test

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 var proto = 'ws'
 var is_local = false
-//if (window.location.hostname === 'defi-nature.onrender.com') {
+/////if (window.location.hostname === 'defi-nature.onrender.com') {
 if (window.location.hostname === 'defi.voxv.repl.co') {
     url = 'defi.voxv.repl.co'
     proto = 'wss'
